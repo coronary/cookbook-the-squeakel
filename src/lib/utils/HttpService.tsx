05929bf80177aca776d447f0be34a90ade8b904d
skbrown333 <skbrown333@gmail.com>
2023-06-18 22:29:05 -0700

    add public env

--- src/lib/utils/HttpService.tsx
+++ src/lib/utils/HttpService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import qs from "qs";
 
-const API_URL = process.env.API_URL ?? "localhost:3001";
+const API_URL = process.env.API_URL ?? process.env.NEXT_PUBLIC_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
