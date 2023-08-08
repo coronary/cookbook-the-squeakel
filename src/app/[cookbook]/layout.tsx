0d4eed531251b0d97375e8b953726239ff192f09
Steffan <skbrown333@gmail.com>
2023-08-07 21:20:24 -0700

    use name url

--- src/app/[cookbook]/layout.tsx
+++ src/app/[cookbook]/layout.tsx
@@ -1,7 +1,6 @@
 import HttpService from "@/lib/utils/HttpService";
 import { Routes } from "@/lib/constants/ApiRoutes";
 import { CookbookLayout } from "@/lib/modules/cookbooks/CookbookLayout";
-import { itemFromUrl } from "@/lib/utils/SectionUtils";
 
 export default async function Layout({
   params,
@@ -10,17 +9,19 @@ export default async function Layout({
   children: React.ReactNode;
   params: any;
 }) {
+  const { cookbook: cookbookName } = params;
   let content = <></>;
   try {
     const games = await HttpService.get(Routes.GAMES_GET_ALL, {
       filters: { name: "melee" },
     });
-    // TODO: update to use from name url
     const cookbooks = await HttpService.get(Routes.COOKBOOK_GET_ALL, {
-      populate: true,
       filters: { game: games[0].id },
     });
-    const cookbook = itemFromUrl(cookbooks, params.cookbook);
+    const cookbook = await HttpService.get(
+      Routes.COOKBOOK_GET_FROM_NAME(cookbookName),
+      { filters: { game: games[0].id } }
+    );
     const guides = cookbook.guides;
 
     content = (
