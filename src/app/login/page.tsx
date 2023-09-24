a5722132702e83b91ec13aafadd9d639a49cc13b
Steffan <skbrown333@gmail.com>
2023-09-24 01:34:22 -0700

    clean up login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,8 @@
 import * as React from "react";
 
 export default function Login() {
-  return (
-    <div className="flex flex-1 bg-slate-300">
-      <a href={"https://api.dev-cookbook.com/login"}>login</a>
-    </div>
-  );
+  React.useEffect(() => {
+    window.location.href = "https://api.dev-cookbook.com/login";
+  }, []);
+  return <div></div>;
 }
