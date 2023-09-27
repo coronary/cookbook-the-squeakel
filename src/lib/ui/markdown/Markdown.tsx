63ed2b115687565adbe1f5ce9976c6579df7cd0c
Steffan <skbrown333@gmail.com>
2023-09-27 09:31:07 -0700

    fix(Markdown): add html support

--- src/lib/ui/markdown/Markdown.tsx
+++ src/lib/ui/markdown/Markdown.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import ReactMarkdown from "react-markdown";
 import { initialDoc } from "../editor/initialDoc";
 import markdownComponents from "./components";
-
+import rehypeRaw from 'rehype-raw';
 import styles from "./Markdown.module.css";
 import { GifPlugin } from "./plugins/GifPlugin";
 import remarkGfm from "./plugins/GFM";
@@ -12,7 +12,7 @@ export const Markdown = ({ body }: { body?: string }) => {
     <ReactMarkdown
       components={markdownComponents}
       remarkPlugins={[remarkGfm]}
-      rehypePlugins={[GifPlugin]}
+      rehypePlugins={[GifPlugin, rehypeRaw as any]}
     >
       {body ?? initialDoc}
     </ReactMarkdown>
