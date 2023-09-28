f8aef5b5a72ac78104e4d432464056c4fae1aa41
Steffan <skbrown333@gmail.com>
2023-09-28 10:30:49 -0700

    chore(env): use API_URL env

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import * as React from "react";
 
+const API_URL = process.env.API_URL ?? process.env.NEXT_PUBLIC_API_URL;
+
 export default function Login() {
   React.useEffect(() => {
-    window.location.href = "https://api.dev-cookbook.com/login";
+    window.location.href = `${API_URL}/login`;
   }, []);
   return <div></div>;
 }
